6912bcbce8f9bd713ac5a40e4d9e826e552a20ac
ThaiQ <quachhongthai200@gmail.com>
2021-01-22 18:45:19 -0800

    Updates

---
+++ precache-manifest.393718f576b0aad5fa7ef8d23a834fee.js
@@ -0,0 +1,242 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1d9c91ee47a562c5e42a4152367e8e0",
+    "url": "/EnglishLearner/index.html"
+  },
+  {
+    "revision": "d3485a0a685ac16b2a21",
+    "url": "/EnglishLearner/static/css/main.58ac992f.chunk.css"
+  },
+  {
+    "revision": "32e3164c9c0e53885ef2",
+    "url": "/EnglishLearner/static/js/2.15c04405.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/EnglishLearner/static/js/2.15c04405.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3485a0a685ac16b2a21",
+    "url": "/EnglishLearner/static/js/main.2f6d11bc.chunk.js"
+  },
+  {
+    "revision": "6fce10b2361c7899f128",
+    "url": "/EnglishLearner/static/js/runtime-main.ee6d0a08.js"
+  },
+  {
+    "revision": "314b1b719e35ce03d08af620798675fb",
+    "url": "/EnglishLearner/static/media/1.314b1b71.mp3"
+  },
+  {
+    "revision": "5252848f88b7b69c9c02bda7de0afe67",
+    "url": "/EnglishLearner/static/media/1.5252848f.mp3"
+  },
+  {
+    "revision": "5d47c5daa8bfe2214153f302fe522605",
+    "url": "/EnglishLearner/static/media/1.5d47c5da.mp3"
+  },
+  {
+    "revision": "66dc44fc06b8254fdeb85db0186fe85e",
+    "url": "/EnglishLearner/static/media/1.66dc44fc.mp3"
+  },
+  {
+    "revision": "68c9f0cd01ceb8111f46969dffaebdba",
+    "url": "/EnglishLearner/static/media/1.68c9f0cd.mp3"
+  },
+  {
+    "revision": "6f17b4f35cedfeac50a664f297d0fbac",
+    "url": "/EnglishLearner/static/media/1.6f17b4f3.mp3"
+  },
+  {
+    "revision": "72d69cd283fe7032b9211311a9a6e0a8",
+    "url": "/EnglishLearner/static/media/1.72d69cd2.mp3"
+  },
+  {
+    "revision": "760d53eaf221920a4f86ef6cd3d18ee1",
+    "url": "/EnglishLearner/static/media/1.760d53ea.mp3"
+  },
+  {
+    "revision": "7c4bc5421aeb48a2f3497b56f0fb1fb9",
+    "url": "/EnglishLearner/static/media/1.7c4bc542.mp3"
+  },
+  {
+    "revision": "9f5189407cc6ae465377e47be9783c40",
+    "url": "/EnglishLearner/static/media/1.9f518940.mp3"
+  },
+  {
+    "revision": "b836a4634c64f892854ccabda61e44ba",
+    "url": "/EnglishLearner/static/media/1.b836a463.mp3"
+  },
+  {
+    "revision": "cfe3335af50d3f4aac95d785ecf0c18f",
+    "url": "/EnglishLearner/static/media/1.cfe3335a.mp3"
+  },
+  {
+    "revision": "d94fc035c8a3736b0a1ffb3bbc03f618",
+    "url": "/EnglishLearner/static/media/1.d94fc035.mp3"
+  },
+  {
+    "revision": "db661445e9d48277fb588d026351e8e1",
+    "url": "/EnglishLearner/static/media/1.db661445.mp3"
+  },
+  {
+    "revision": "e4029797e3a5443acf2bd84fa71ebd7e",
+    "url": "/EnglishLearner/static/media/1.e4029797.mp3"
+  },
+  {
+    "revision": "f637a1ec423f25c6fb2ab382c56cb513",
+    "url": "/EnglishLearner/static/media/1.f637a1ec.mp3"
+  },
+  {
+    "revision": "baa50293bfd908370438e7aeacdfd91c",
+    "url": "/EnglishLearner/static/media/10.baa50293.mp3"
+  },
+  {
+    "revision": "d2cad1b1fbfbc64ac240c0881eb471bf",
+    "url": "/EnglishLearner/static/media/11.d2cad1b1.mp3"
+  },
+  {
+    "revision": "f5ac1977431522aa381755f6c1119a7f",
+    "url": "/EnglishLearner/static/media/12.f5ac1977.mp3"
+  },
+  {
+    "revision": "eb36d60aa6a2e4669a266366d2a617c0",
+    "url": "/EnglishLearner/static/media/13.eb36d60a.mp3"
+  },
+  {
+    "revision": "2acecedcb1d68a14dc95a0be13ebaeac",
+    "url": "/EnglishLearner/static/media/14.2acecedc.mp3"
+  },
+  {
+    "revision": "6e686007ddcc63e7f5ec74ccbcb93dde",
+    "url": "/EnglishLearner/static/media/15.6e686007.mp3"
+  },
+  {
+    "revision": "51bfe5bbb4f1546d947effc98f26db23",
+    "url": "/EnglishLearner/static/media/16.51bfe5bb.mp3"
+  },
+  {
+    "revision": "bb84dafea18b6984b166c52ec6b3f3cf",
+    "url": "/EnglishLearner/static/media/17.bb84dafe.mp3"
+  },
+  {
+    "revision": "0f51fad6eec18dd9686654807ace6a19",
+    "url": "/EnglishLearner/static/media/18.0f51fad6.mp3"
+  },
+  {
+    "revision": "6b811d7ce9e7666ea864da70aa43a385",
+    "url": "/EnglishLearner/static/media/2.6b811d7c.mp3"
+  },
+  {
+    "revision": "d2be8740c72ad5324966e7353c2669e6",
+    "url": "/EnglishLearner/static/media/3.d2be8740.mp3"
+  },
+  {
+    "revision": "f65c8f9808901924cc72f6a6682f3b39",
+    "url": "/EnglishLearner/static/media/4.f65c8f98.mp3"
+  },
+  {
+    "revision": "18c3d71782b3f9557f779e074f524e56",
+    "url": "/EnglishLearner/static/media/5.18c3d717.mp3"
+  },
+  {
+    "revision": "f607fc39d4c89dee6bc34d7b7cdcda91",
+    "url": "/EnglishLearner/static/media/6.f607fc39.mp3"
+  },
+  {
+    "revision": "6b63d02ba6e78f0bb3f79136a575fed7",
+    "url": "/EnglishLearner/static/media/7.6b63d02b.mp3"
+  },
+  {
+    "revision": "4742c146a0975802a393115d49a1b9b1",
+    "url": "/EnglishLearner/static/media/8.4742c146.mp3"
+  },
+  {
+    "revision": "2dc99fc2e7b3de7d5b354d65ebb72c5a",
+    "url": "/EnglishLearner/static/media/9.2dc99fc2.mp3"
+  },
+  {
+    "revision": "182c80873c6c12087e4acda7067ef2d0",
+    "url": "/EnglishLearner/static/media/reading.182c8087.pdf"
+  },
+  {
+    "revision": "1e12a3d9c2133724b69a697626ad4bc4",
+    "url": "/EnglishLearner/static/media/reading.1e12a3d9.pdf"
+  },
+  {
+    "revision": "2c0c3c9424663e60c5774699420b0fa5",
+    "url": "/EnglishLearner/static/media/reading.2c0c3c94.pdf"
+  },
+  {
+    "revision": "4655d71e18cc65a4ea0a39fefcd42c19",
+    "url": "/EnglishLearner/static/media/reading.4655d71e.pdf"
+  },
+  {
+    "revision": "4a6627eadaa6c35ae0bcb000ee1922cf",
+    "url": "/EnglishLearner/static/media/reading.4a6627ea.pdf"
+  },
+  {
+    "revision": "4ca4b9fdcdbde11734c1e8a509542259",
+    "url": "/EnglishLearner/static/media/reading.4ca4b9fd.pdf"
+  },
+  {
+    "revision": "4fa5c1ef9b7bc8c348dff687527267b5",
+    "url": "/EnglishLearner/static/media/reading.4fa5c1ef.pdf"
+  },
+  {
+    "revision": "5148d788b9aabc7c665a678d6abf7ae3",
+    "url": "/EnglishLearner/static/media/reading.5148d788.pdf"
+  },
+  {
+    "revision": "55edfa99505992fbcd9b12f6ef1d9c86",
+    "url": "/EnglishLearner/static/media/reading.55edfa99.pdf"
+  },
+  {
+    "revision": "5b687bcbbacf078354534bb8941c4172",
+    "url": "/EnglishLearner/static/media/reading.5b687bcb.pdf"
+  },
+  {
+    "revision": "7c05e3e8de5d0caae9af1948a0151afd",
+    "url": "/EnglishLearner/static/media/reading.7c05e3e8.pdf"
+  },
+  {
+    "revision": "952259527ad4f5fd1c551c878d1e8929",
+    "url": "/EnglishLearner/static/media/reading.95225952.pdf"
+  },
+  {
+    "revision": "a3d72d5b4b6e698c8d81ff477a767b69",
+    "url": "/EnglishLearner/static/media/reading.a3d72d5b.pdf"
+  },
+  {
+    "revision": "bfb5d52a858cf1db08eb899b33d19115",
+    "url": "/EnglishLearner/static/media/reading.bfb5d52a.pdf"
+  },
+  {
+    "revision": "ca2e5c7046bc8bc724a6b2184b59c086",
+    "url": "/EnglishLearner/static/media/reading.ca2e5c70.pdf"
+  },
+  {
+    "revision": "dca2834c2739f5cd672b0a7a56c2bf0a",
+    "url": "/EnglishLearner/static/media/reading.dca2834c.pdf"
+  },
+  {
+    "revision": "e247cc58ed73c1bfee99f6606c0cca30",
+    "url": "/EnglishLearner/static/media/reading.e247cc58.pdf"
+  },
+  {
+    "revision": "e3ea46b0fd2c8b1fd42106ef781a002a",
+    "url": "/EnglishLearner/static/media/reading.e3ea46b0.pdf"
+  },
+  {
+    "revision": "e5db58c989cd36c557296a6d89b224c3",
+    "url": "/EnglishLearner/static/media/reading.e5db58c9.pdf"
+  },
+  {
+    "revision": "eebf7e1c632895ba835f060dde04fa73",
+    "url": "/EnglishLearner/static/media/reading.eebf7e1c.pdf"
+  },
+  {
+    "revision": "f2be145095a702b744812e8b300bc407",
+    "url": "/EnglishLearner/static/media/reading.f2be1450.pdf"
+  }
+]);
